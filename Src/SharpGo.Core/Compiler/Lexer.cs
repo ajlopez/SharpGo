5acd18b9811817af5ac7bd0f3aefce1e989bb9a5
ajlopez <webmaster@ajlopez.com>
2014-05-01 08:37:00 -0300

    Lexer get name test in green, naive implementation

--- Src/SharpGo.Core/Compiler/Lexer.cs
+++ Src/SharpGo.Core/Compiler/Lexer.cs
@@ -7,13 +7,23 @@
 
     public class Lexer
     {
+        private string text;
+
         public Lexer(string text)
         {
+            this.text = text;
         }
 
         public Token NextToken()
         {
-            return null;
+            if (this.text == null)
+                return null;
+
+            var token = new Token(TokenType.Name, this.text);
+
+            this.text = null;
+
+            return token;
         }
     }
 }
