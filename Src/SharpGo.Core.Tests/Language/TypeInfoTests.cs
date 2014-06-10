55cbc67d2b9bbb2f263737a880653ab9bb7bd4ee
ajlopez <webmaster@ajlopez.com>
2014-06-10 16:05:25 -0300

    TypeInfo first tests

---
+++ Src/SharpGo.Core.Tests/Language/TypeInfoTests.cs
@@ -0,0 +1,28 @@
+﻿namespace SharpGo.Core.Tests.Language
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SharpGo.Core.Language;
+
+    [TestClass]
+    public class TypeInfoTests
+    {
+        [TestMethod]
+        public void GetBoolTypeInfoFromTrue()
+        {
+            var result = TypeInfo.GetTypeInfo(true);
+
+            Assert.IsNotNull(result);
+            Assert.AreSame(TypeInfo.Bool, result);
+        }
+
+        [TestMethod]
+        public void GetStringTypeInfo()
+        {
+            var result = TypeInfo.GetTypeInfo("foo");
+
+            Assert.IsNotNull(result);
+            Assert.AreSame(TypeInfo.String, result);
+        }
+    }
+}
---
+++ Src/SharpGo.Core/Language/TypeInfo.cs
@@ -0,0 +1,35 @@
+﻿namespace SharpGo.Core.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class TypeInfo
+    {
+        private static TypeInfo tibool = new TypeInfo("bool");
+        private static TypeInfo tistring = new TypeInfo("string");
+
+        private string name;
+
+        public TypeInfo(string name)
+        {
+            this.name = name;
+        }
+
+        public static TypeInfo Bool { get { return tibool; } }
+
+        public static TypeInfo String { get { return tistring; } }
+
+        public static TypeInfo GetTypeInfo(object value)
+        {
+            if (value is bool)
+                return tibool;
+
+            if (value is string)
+                return tistring;
+
+            throw new NotImplementedException();
+        }
+    }
+}
