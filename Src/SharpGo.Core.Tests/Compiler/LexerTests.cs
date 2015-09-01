c168748df66af4876026e5d67f79ff8a731c7f47
ajlopez <webmaster@ajlopez.com>
2015-09-01 07:27:35 -0300

    Double quote in string

--- Src/SharpGo.Core.Tests/Compiler/LexerTests.cs
+++ Src/SharpGo.Core.Tests/Compiler/LexerTests.cs
@@ -351,6 +351,20 @@
         }
 
         [TestMethod]
+        public void GetStringWithDoubleQuote()
+        {
+            Lexer lexer = new Lexer("\"foo\\\"bar\"");
+
+            var token = lexer.NextToken();
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual(TokenType.String, token.Type);
+            Assert.AreEqual("foo\"bar", token.Value);
+
+            Assert.IsNull(lexer.NextToken());
+        }
+
+        [TestMethod]
         public void GetBracketsAsDelimiters()
         {
             Lexer lexer = new Lexer("{}");
