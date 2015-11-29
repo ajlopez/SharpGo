af217604b3f572a122c939fcdbf09845b05e7ffb
ajlopez <webmaster@ajlopez.com>
2015-11-29 07:38:43 -0300

    StructMemberInfo

---
+++ Src/SharpGo.Core.Tests/Language/StructTypeInfoTests.cs
@@ -0,0 +1,22 @@
+﻿namespace SharpGo.Core.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SharpGo.Core.Language;
+
+    [TestClass]
+    public class StructTypeInfoTests
+    {
+        [TestMethod]
+        public void CreateStructMemberInfo()
+        {
+            var sm = new StructMemberInfo("name", TypeInfo.String);
+
+            Assert.AreEqual("name", sm.Name);
+            Assert.AreSame(TypeInfo.String, sm.TypeInfo);
+        }
+    }
+}
