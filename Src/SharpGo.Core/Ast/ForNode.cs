441891e441926f0f204395663fb7111448bc8fb8
ajlopez <webmaster@ajlopez.com>
2015-12-07 07:08:38 -0300

    Refactor for node constructor

--- Src/SharpGo.Core/Ast/ForNode.cs
+++ Src/SharpGo.Core/Ast/ForNode.cs
@@ -7,16 +7,29 @@
 
     public class ForNode : IStatementNode
     {
+        private IStatementNode initstmt;
         private IExpressionNode expression;
+        private IStatementNode poststmt;
         private BlockNode block;
 
         public ForNode(IExpressionNode expression, BlockNode block)
+            : this(null, expression, null, block)
         {
+        }
+
+        public ForNode(IStatementNode initstmt, IExpressionNode expression, IStatementNode poststmt, BlockNode block)
+        {
+            this.initstmt = initstmt;
             this.expression = expression;
+            this.poststmt = poststmt;
             this.block = block;
         }
 
+        public IStatementNode InitStatement { get { return this.initstmt; } }
+
         public IExpressionNode Expression { get { return this.expression; } }
+
+        public IStatementNode PostStatement { get { return this.poststmt; } }
 
         public BlockNode BlockNode { get { return this.block; } }
     }
