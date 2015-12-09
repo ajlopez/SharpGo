4203f55fb2d3b29ce016d19239da1e7bb0af7868
ajlopez <webmaster@ajlopez.com>
2015-12-09 06:47:31 -0300

    DotNode create test

---
+++ Src/SharpGo.Core.Tests/Ast/DotNodeTests.cs
@@ -0,0 +1,21 @@
+﻿namespace SharpGo.Core.Tests.Ast
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SharpGo.Core.Ast;
+    using SharpGo.Core.Language;
+
+    [TestClass]
+    public class DotNodeTests
+    {
+        [TestMethod]
+        public void CreateDotNode()
+        {
+            var expr = new NameNode("foo");
+            var node = new DotNode(expr, "bar");
+
+            Assert.AreSame(expr, node.ExpressionNode);
+            Assert.AreEqual("bar", node.Name);
+        }
+    }
+}
---
+++ Src/SharpGo.Core/Ast/DotNode.cs
@@ -0,0 +1,29 @@
+﻿namespace SharpGo.Core.Ast
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using SharpGo.Core.Language;
+
+    public class DotNode : IExpressionNode
+    {
+        private IExpressionNode expression;
+        private string name;
+
+        public DotNode(IExpressionNode expression, string name)
+        {
+            this.expression = expression;
+            this.name = name;
+        }
+
+        public IExpressionNode ExpressionNode { get { return this.expression; } }
+
+        public string Name { get { return this.name; } }
+
+        public TypeInfo TypeInfo
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
