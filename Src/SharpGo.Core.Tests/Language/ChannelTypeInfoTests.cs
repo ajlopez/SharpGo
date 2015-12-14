d47d4976c043e734763b5d924e2aaffcc1ae8ed3
ajlopez <webmaster@ajlopez.com>
2015-12-14 06:44:40 -0300

    ChannelTypeInfo first test

---
+++ Src/SharpGo.Core.Tests/Language/ChannelTypeInfoTests.cs
@@ -0,0 +1,22 @@
+﻿namespace SharpGo.Core.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SharpGo.Core.Language;
+
+    [TestClass]
+    public class ChannelTypeInfoTests
+    {
+        [TestMethod]
+        public void CreateWithSameType()
+        {
+            var type = new ChannelTypeInfo(TypeInfo.Int);
+
+            Assert.AreSame(TypeInfo.Int, type.ReceiveTypeInfo);
+            Assert.AreSame(TypeInfo.Int, type.SendTypeInfo);
+        }
+    }
+}
---
+++ Src/SharpGo.Core/Language/ChannelTypeInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace SharpGo.Core.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class ChannelTypeInfo : TypeInfo
+    {
+        private TypeInfo sendtypeinfo;
+        private TypeInfo receivetypeinfo;
+
+        public ChannelTypeInfo(TypeInfo typeinfo)
+            : base("channel")
+        {
+            this.sendtypeinfo = typeinfo;
+            this.receivetypeinfo = typeinfo;
+        }
+
+        public TypeInfo ReceiveTypeInfo { get { return this.receivetypeinfo; } }
+
+        public TypeInfo SendTypeInfo { get { return this.sendtypeinfo; } }
+    }
+}
