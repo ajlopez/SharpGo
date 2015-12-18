6e3f162a227681328fee9958bcdcef8e8addcb7f
ajlopez <webmaster@ajlopez.com>
2015-12-18 06:49:32 -0300

    MapTypeInfo first creation test

---
+++ Src/SharpGo.Core.Tests/Language/MapTypeInfoTests.cs
@@ -0,0 +1,22 @@
+﻿namespace SharpGo.Core.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SharpGo.Core.Language;
+
+    [TestClass]
+    public class MapTypeInfoTests
+    {
+        [TestMethod]
+        public void CreateWithTypes()
+        {
+            var type = new MapTypeInfo(TypeInfo.String, TypeInfo.Int);
+
+            Assert.AreSame(TypeInfo.String, type.KeyTypeInfo);
+            Assert.AreSame(TypeInfo.Int, type.ElementTypeInfo);
+        }
+    }
+}
