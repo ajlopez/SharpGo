1316653d8b3d2e21188c9daac29f181dc6d5055d
ajlopez <webmaster@ajlopez.com>
2016-01-24 11:20:36 -0300

    Parser internal refactor, process shift binary operators

--- Src/SharpGo.Core/Compiler/Parser.cs
+++ Src/SharpGo.Core/Compiler/Parser.cs
@@ -11,7 +11,7 @@
     public class Parser
     {
         private static Dictionary<string, TypeInfo> types = new Dictionary<string, TypeInfo>() { { "int", TypeInfo.Int }, { "int32", TypeInfo.Int32 }, { "uint", TypeInfo.UInt }, { "rune", TypeInfo.Int32 }, { "int16", TypeInfo.Int16 }, { "int64", TypeInfo.Int64 }, { "float32", TypeInfo.Float32 }, { "float64", TypeInfo.Float64 }, { "string", TypeInfo.String }, { "bool", TypeInfo.Bool }, { "nil", TypeInfo.Nil }, { "complex64", TypeInfo.Complex64 }, { "complex128", TypeInfo.Complex128 } };
-        private static Dictionary<string, BinaryOperator> bops = new Dictionary<string, BinaryOperator>() { { "+", BinaryOperator.Add }, { "-", BinaryOperator.Substract }, { "*", BinaryOperator.Multiply }, { "/", BinaryOperator.Divide }, { "%", BinaryOperator.Mod }, { "<", BinaryOperator.Less }, { "<=", BinaryOperator.LessEqual }, { ">", BinaryOperator.Greater }, { ">=", BinaryOperator.GreaterEqual } };
+        private static Dictionary<string, BinaryOperator> bops = new Dictionary<string, BinaryOperator>() { { "+", BinaryOperator.Add }, { "-", BinaryOperator.Substract }, { "*", BinaryOperator.Multiply }, { "/", BinaryOperator.Divide }, { "%", BinaryOperator.Mod }, { "<", BinaryOperator.Less }, { "<=", BinaryOperator.LessEqual }, { ">", BinaryOperator.Greater }, { ">=", BinaryOperator.GreaterEqual }, { "<<", BinaryOperator.LeftShift }, { ">>", BinaryOperator.RightShift } };
 
         private static string[][] binaryoperators = new string[][] 
         {
@@ -156,10 +156,6 @@
             {
                 if (bops.ContainsKey(token.Value))
                     expr = new BinaryNode(expr, bops[token.Value], this.ParseBinaryExpressionNode(level + 1));
-                else if (token.Value == "<<")
-                    expr = new BinaryNode(expr, BinaryOperator.LeftShift, this.ParseBinaryExpressionNode(level + 1));
-                else if (token.Value == ">>")
-                    expr = new BinaryNode(expr, BinaryOperator.RightShift, this.ParseBinaryExpressionNode(level + 1));
                 else if (token.Value == "&")
                     expr = new BinaryNode(expr, BinaryOperator.BitwiseAnd, this.ParseBinaryExpressionNode(level + 1));
                 else if (token.Value == "|")
