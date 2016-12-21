d80d50ec356eeb075cee731a99e97095b9fe0b7a
ajlopez <webmaster@ajlopez.com>
2016-12-21 18:42:25 -0300

    UnaryNode, UnaryOperator, WIP

---
+++ Src/SharpGo.Core/Ast/UnaryNode.cs
@@ -0,0 +1,29 @@
+﻿namespace SharpGo.Core.Ast
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using SharpGo.Core.Language;
+
+    public class UnaryNode : IExpressionNode
+    {
+        private IExpressionNode exprnode;
+        private UnaryOperator @operator;
+
+        public UnaryNode(IExpressionNode exprnode, UnaryOperator @operator)
+        {
+            this.exprnode = exprnode;
+            this.@operator = @operator;
+        }
+
+        public IExpressionNode ExpressionNode { get { return this.exprnode; } }
+
+        public UnaryOperator Operator { get { return this.@operator; } }
+
+        public TypeInfo TypeInfo
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
