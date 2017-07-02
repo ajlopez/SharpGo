bc940251166a20e4c0d8f7d8cbdc4391c899b18c
ajlopez <webmaster@ajlopez.com>
2017-07-02 18:53:50 -0300

    TypedArray first implementation

---
+++ Src/SharpGo.Core.Tests/Language/TypeArrayTests.cs
@@ -0,0 +1,34 @@
+﻿namespace SharpGo.Core.Tests.Language
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SharpGo.Core.Language;
+
+    [TestClass]
+    public class TypeArrayTests
+    {
+        [TestMethod]
+        public void CreateIntegerArray()
+        {
+            var array = new TypedArray<int>(10);
+
+            Assert.IsNotNull(array);
+            Assert.AreEqual(10, array.Length);
+
+            for (int k = 0; k < array.Length; k++)
+                Assert.AreEqual(0, array[k]);
+        }
+
+        [TestMethod]
+        public void SetAndGetValuesUsingIntegerArray()
+        {
+            var array = new TypedArray<int>(10);
+
+            for (int k = 0; k < array.Length; k++)
+                array[k] = k + 1;
+
+            for (int k = 0; k < array.Length; k++)
+                Assert.AreEqual(k + 1, array[k]);
+        }
+    }
+}
---
+++ Src/SharpGo.Core/Language/TypedArray.cs
@@ -0,0 +1,32 @@
+﻿namespace SharpGo.Core.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class TypedArray<T>
+    {
+        private T[] values;
+
+        public TypedArray(int size)
+        {
+            this.values = new T[size];
+        }
+
+        public int Length { get { return this.values.Length; } }
+
+        public T this[int index]
+        {
+            get
+            {
+                return this.values[index];
+            }
+
+            set
+            {
+                this.values[index] = value;
+            }
+        }
+    }
+}
